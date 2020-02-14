bd4d67901efe9a90948407a1c2113ae04fb2f2d0
Hamish Harrison <510299@ACDM.DS.SAIT.CA>
2020-02-14 01:26:56 -0700

    Added customers view and connection string
    Added a customers DB that will show the current customers information
    Also added the connectionstring to the web.config file (as well as the connectionstring reference class called TravelExpertsDB)

--- Team6Workshop5/Team6Workshop5/Models/Customer.cs
+++ Team6Workshop5/Team6Workshop5/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,19 +9,54 @@ namespace Team6Workshop5.Models
 {
     public class Customer
     {
+        [Required]
+        [Key]
+        [Display(Name = "Customer ID")]
         public int CustomerId { get; set; }
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "First Name")]
         public string CustFirstName { get; set; }
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "Last Name")]
         public string CustLastName { get; set; }
+        [Required]
+        [StringLength(75)]
+        [Display(Name = "Address")]
         public string CustAddress { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "City")]
         public string CustCity { get; set; }
+        [Required]
+        [StringLength(2)]
+        [Display(Name = "Province")]
         public string CustProv { get; set; }
+        [Required]
+        [StringLength(7)]
+        [Display(Name = "Postal Code")]
         public string CustPostal { get; set; }
+        [StringLength(25)]
+        [Display(Name = "Country")]
         public string CustCountry { get; set; }
+        [StringLength(20)]
+        [Display(Name = "Home Phone")]
         public string CustHomePhone { get; set; }
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "Business Phone")]
         public string CustBusPhone { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Email")]
         public string CustEmail { get; set; }
+        [ForeignKey("Agents")]
+        [Display(Name = "Agent ID")]
         public int AgentId { get; set; }
+        [StringLength(25)]
         public string UserName { get; set; }
+        [StringLength(50)]
         public string Password { get; set; }
 
     }
---
+++ Team6Workshop5/Team6Workshop5/Models/CustomerDB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Team6Workshop5.Models
+{
+    public class CustomerDB
+    {
+        public static Customer GetCustomerDetails(int custId)
+        {
+            Customer cust = null;
+            string query = "SELECT CustFirstName, CustLastName, CustAddress, CustCity, CustProv, CustPostal, CustCountry, " +
+                           "CustHomePhone, CustBusPhone, CustEmail, AgentId, UserName " +
+                           "FROM Customers " +
+                           "WHERE CustomerId = @CustomerId";
+
+            using (SqlConnection connection = new SqlConnection(TravelExpertsDB.GetConnectionString()))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerId", custId);
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    while (reader.Read())
+                    {
+                        cust = new Customer();
+                        cust.CustFirstName = reader["CustFirstName"].ToString();
+                        cust.CustLastName = reader["CustLastName"].ToString();
+                        cust.CustAddress = reader["CustAddress"].ToString();
+                        cust.CustCity = reader["CustCity"].ToString();
+                        cust.CustProv = reader["CustProv"].ToString();
+                        cust.CustPostal = reader["CustPostal"].ToString();
+                        cust.CustCountry = reader["CustCountry"].ToString();
+                        cust.CustHomePhone = reader["CustHomePhone"].ToString();
+                        cust.CustBusPhone = reader["CustBusPhone"].ToString();
+                        cust.CustEmail = reader["CustEmail"].ToString();
+                        cust.AgentId = Convert.ToInt32(reader["AgentId"]);
+                        cust.UserName = reader["UserName"].ToString();
+
+                    }
+                }
+            }
+            return cust;
+        }
+    }
+}
