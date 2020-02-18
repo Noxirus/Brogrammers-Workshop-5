306f3b29adacc6462b04cf6097d68835a9fbd7d1
Noxirus <hamishharrison@hotmail.com>
2020-02-18 12:02:13 -0700

    Added annotations and formatting to bookings

--- Team6Workshop5/Team6Workshop5/Models/NEWAccountBookings.cs
+++ Team6Workshop5/Team6Workshop5/Models/NEWAccountBookings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,18 +10,29 @@ namespace Team6Workshop5.Models
     {
 
         //PACKAGES
-       
-            public string PACKCustFirstName { get; set; }
-            public string PACKProdName { get; set; }
-            public decimal? PACKBasePrice { get; set; }
-            public string PACKPkgName { get; set; }
-            public decimal? PACKPkgBasePrice { get; set; }
-            public int? PACKBookingId { get; set; }
-            public decimal? PACKTotal { get; set; }
-            public string PRODCustFirstName { get; set; }
-            public string PRODProdName { get; set; }
-            public decimal? PRODBasePrice { get; set; }
-            public int? PRODBookingId { get; set; }
-            public decimal? PRODTotal { get; set; }
+
+        [Display(Name = "")]
+        public string PACKCustFirstName { get; set; }
+        [Display(Name = "Product Name")]
+        public string PACKProdName { get; set; }
+        [Display(Name = "Price")]
+        public decimal? PACKBasePrice { get; set; }
+        [Display(Name = "Package Name")]
+        public string PACKPkgName { get; set; }
+        [Display(Name = "Price")]
+        public decimal? PACKPkgBasePrice { get; set; }
+        [Display(Name = "Booking ID")]
+        public int? PACKBookingId { get; set; }
+        [Display(Name = "Total")]
+        public decimal? PACKTotal { get; set; }
+        public string PRODCustFirstName { get; set; }
+        [Display(Name = "Product Name")]
+        public string PRODProdName { get; set; }
+        [Display(Name = "Price")]
+        public decimal? PRODBasePrice { get; set; }
+        [Display(Name = "Booking ID")]
+        public int? PRODBookingId { get; set; }
+        [Display(Name = "Total")]
+        public decimal? PRODTotal { get; set; }
     }
 }
